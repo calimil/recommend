63b43aab176a20ec9cddd4573919961fe8adc4ad
calimil <3564064964@qq.com>
2025-10-16 15:43:59 +0800

    apps

--- recommend_profession/apps.py
+++ recommend_profession/apps.py
@@ -1,5 +1,7 @@
+# recommend_profession/apps.py
 from django.apps import AppConfig
 
-
+# 定义应用配置类，继承自Django的AppConfig基类
 class RecommendProfessionConfig(AppConfig):
+    # 应用的名称，必须与应用目录名一致
     name = 'recommend_profession'
